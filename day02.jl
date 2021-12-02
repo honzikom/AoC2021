054c351f182f1e364f744da3fb0e652fb430f971
Darwin <jan.bohm.93@gmail.com>
2021-12-02 13:16:27 +0100

    Add files via upload

---
+++ day02.jl
@@ -0,0 +1,33 @@
+cd("/home/janbohm/Dokumenty/AoC/02")
+using Pkg
+using CSV
+using DataFrames
+
+x = CSV.read("./input/day2.csv",DataFrame, header = 0)
+rename!(x, ["Direction", "Value"])
+
+# Part 1
+
+horizontal = sum(x.Value[x.Direction .== "forward"])
+depth = sum(x.Value[x.Direction .== "down"]) - sum(x.Value[x.Direction .== "up"])
+
+println("The result is: ", horizontal * depth)
+
+# Part 2
+h = 0
+d = 0
+a = 0
+
+x = Matrix(x)
+for ind in 1:(size(x)[1])
+    if x[ind,1] == "forward"
+        h += x[ind,2]
+        d += a*x[ind,2]
+    elseif  x[ind,1] == "down"
+        a += x[ind,2]
+    elseif x[ind,1] == "up"
+        a -= x[ind,2]
+    end
+end
+
+println("The result is: ", h * d)
